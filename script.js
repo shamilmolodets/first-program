223682cddc9d80363e5f9be4978acae90afa6efd
Shamil <example@gmail.com>
2021-01-13 17:21:57 +0200

    decision

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 const numberOfFilms = +prompt ('Сколько фильмов вы уже просмотрели?', '');
 
 const personalMovieDB = {
@@ -19,8 +20,40 @@ for(let i = 0; i <2; i++) {
             console.log('error');
             i--;
         }
-
 }
+
+/* let i = 0; */
+
+/* while (i < 2) {
+    
+    const       a = prompt("Один из последних просмотренных фильмов?", ''),
+                b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    i++;
+} */
+
+/* do {
+    const       a = prompt("Один из последних просмотренных фильмов?", ''),
+                b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    i++;
+}
+while (i < 2); */
+
 if (personalMovieDB.count < 10) {
     console.log("Просмотренно довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
